a2cfb6a5da8049b39a1db8884cc4e0ed4a28e49a
Tom Whitbread <tom@gript.co.uk>
2015-10-19 12:48:26 +0000

    adding functions page and ng-app to index

--- wp-content/themes/angularjs/index.php
+++ wp-content/themes/angularjs/index.php
@@ -1,20 +1,6 @@
-
-<?php
-
-function my_scripts() {
-
-	wp_enqueue_script(
-		'angularjs',
-		get_stylesheet_directory_uri() . '/node_modules/angular/angular.min.js'
-	);
-
-	wp_enqueue_script(
-		'angularjs-route',
-		get_stylesheet_directory_uri() . '/node_modules/angular-route/angular-route.min.js'
-	);
-
-}
-
-add_action( 'wp_enqueue_scripts', 'my_scripts' );
-
-?>
+<!DOCTYPE html>
+<html ng-app>
+<head>
+	<title>AngularJS Demo Theme</title>
+	<?php wp_head(); ?>
+</head>
