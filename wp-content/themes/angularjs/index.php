aa7270768c3a3c9a0ca105ec1791394e182c866b
tomtom87 <tom@gript.co.uk>
2015-10-22 02:25:06 +0000

    updating modules and fixing the url for angular

--- wp-content/themes/angularjs/index.php
+++ wp-content/themes/angularjs/index.php
@@ -1,6 +1,21 @@
 <!DOCTYPE html>
 <html ng-app>
 <head>
-	<title>AngularJS Demo Theme</title>
+	<title><?php wp_title(); ?></title>
 	<?php wp_head(); ?>
 </head>
+
+<body>
+
+	<header>
+		<h1>Angular Wp-Theme Demo</h1>
+	</header>
+
+	<ng-posts></ng-posts>
+
+	<footer>
+		&copy; <?php echo date( 'Y' ); ?>
+	</footer>
+
+</body>
+</html>
